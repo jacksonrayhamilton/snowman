eab0cd4e30b4fd2de83892ab90685877fcd4691d
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2014-10-20 02:28:09 -0700

    Add bower.json, add grunt-release.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,11 @@
 
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-jslint');
-    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-release');
 
     grunt.initConfig({
 
